42112fe2af2a9f62c24bd6c895dff988dbb9cb88
IAFEnvoy <tanksheng545@126.com>
2025-10-09 11:33:22 +0800

    fix #1538 fix #1532 tag related

--- src/main/java/com/github/alexmodguy/alexscaves/mixin/FoodDataMixin.java
+++ src/main/java/com/github/alexmodguy/alexscaves/mixin/FoodDataMixin.java
@@ -2,11 +2,14 @@ package com.github.alexmodguy.alexscaves.mixin;
 
 import com.github.alexmodguy.alexscaves.server.item.PrimordialArmorItem;
 import com.github.alexmodguy.alexscaves.server.misc.ACTagRegistry;
+import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.ForgeItemTagsProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
